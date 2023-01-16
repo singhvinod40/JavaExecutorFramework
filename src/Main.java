34fc54dd57ac848ee0c41b43020b971d944ebb51
Vinod Singh <singhvinod40@gmail.com>
2023-01-17 00:44:37 +0530

    adding executor code

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,60 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
 
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
 
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello world!");
+        Runnable runnable = () -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        };
+
+        //1. execute task using execute() method
+        executorService.execute(runnable);
+        executorService.shutdown();
+
+        Runnable task = () -> {
+            System.out.println("runnable is an functional interface");
+        };
+
+        task.run();
+
+        Thread thread = new Thread(task);
+        thread.start();
+        System.out.println("All task done");
+
+        //callable example
 
 
+        Callable<Integer> calltask =()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                return 123;
+            }
+            catch (InterruptedException e) {
+                throw new IllegalStateException("task interrupted", e);
+            }
 
-            executorService.shutdown();
+        };
 
+
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        Future<Integer> future = executor.submit(calltask);
+
+        System.out.println("future done? " + future.isDone());
+
+        Integer result = future.get();
+
+        System.out.println("future done? " + future.isDone());
+        System.out.print("result: " + result);
     }
 }
